eee5d3adbd59f12d7ee9932980239592c65e9a91
Esonhugh <esonhughoutside@gmail.com>
2023-01-22 08:53:54 -0500

    update(object): ebpf-new

--- pkg/ebpf-new/object.go
+++ pkg/ebpf-new/object.go
@@ -2,24 +2,30 @@ package ebpfnew
 
 import (
 	"ebpf_common/pkg/generate"
+	"fmt"
 
+	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/link"
+	"github.com/cilium/ebpf/rlimit"
 )
 
 type CilliumEBPFRuntime struct {
-	Objects *generate.BpfObjects
+	Objects generate.BpfObjects
 	Links   []link.Link
 }
 
-func CreateCilliumEBPFRuntime() *CilliumEBPFRuntime {
-	return &CilliumEBPFRuntime{
-		Objects: &generate.BpfObjects{},
+func RemoveMemoryLimit() error {
+	return rlimit.RemoveMemlock()
+}
+
+func CreateCilliumEBPFRuntime() (*CilliumEBPFRuntime, error) {
+	var BpfObjects generate.BpfObjects
+	err := generate.LoadBpfObjects(&BpfObjects, nil)
+	if err != nil {
+		return nil, fmt.Errorf("load ebpf Objects error: %w", err)
 	}
-}
-
-func (c *CilliumEBPFRuntime) LoadBpf() {
-}
-
-func (c *CilliumEBPFRuntime) CreateLinks() {
-
+	return &CilliumEBPFRuntime{
+		Objects: BpfObjects,
+		Links:   []link.Link{},
+	}, nil
 }
