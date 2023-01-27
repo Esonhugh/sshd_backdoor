284bcee997565ba983ffc13089a1e9dd5b44cbe9
Esonhugh <esonhughoutside@gmail.com>
2023-01-27 18:24:25 +0800

    bug(unsupported): Attach tracepoint is unsupported. https://github.com/cilium/ebpf/issues/597

--- pkg/ebpf-new/attach_detach.go
+++ pkg/ebpf-new/attach_detach.go
@@ -2,29 +2,31 @@ package ebpfnew
 
 import (
 	"fmt"
-	"github.com/cilium/ebpf/link"
 )
 
 // Attach func create links map and make the program attached to the kernel.
 func (c *CiliumEBPFRuntime) Attach() error {
-	var err error
-	c.Links[BPF_PROG_SYSCALL_ENTER_OPENAT], err = link.AttachTracing(link.TracingOptions{
-		Program: c.Objects.HandleOpenatEnter,
-	})
-	c.Links[BPF_PROG_SYSCALL_EXIT_OPENAT], err = link.AttachTracing(link.TracingOptions{
-		Program: c.Objects.HandleOpenatExit,
-	})
-	c.Links[BPF_PROG_SYSCALL_ENTER_READ], err = link.AttachTracing(link.TracingOptions{
-		Program: c.Objects.HandleReadEnter,
-	})
-	c.Links[BPF_PROG_SYSCALL_EXIT_READ], err = link.AttachTracing(link.TracingOptions{
-		Program: c.Objects.HandleReadExit,
-	})
+	return c.CreateLink()
+	/*
+		var err error
+		c.Links[BPF_PROG_SYSCALL_ENTER_OPENAT], err = link.AttachTracepoint(link.RawTracepointOptions{
+			Program: c.Objects.HandleOpenatEnter,
+		})
+		c.Links[BPF_PROG_SYSCALL_EXIT_OPENAT], err = link.AttachTracepoint(link.RawTracepointOptions{
+			Program: c.Objects.HandleOpenatExit,
+		})
+		c.Links[BPF_PROG_SYSCALL_ENTER_READ], err = link.AttachTracepoint(link.TracingOptions{
+			Program: c.Objects.HandleReadEnter,
+		})
+		c.Links[BPF_PROG_SYSCALL_EXIT_READ], err = link.AttachTracepoint(link.TracingOptions{
+			Program: c.Objects.HandleReadExit,
+		})
 
-	if err != nil {
-		return fmt.Errorf("attach link error: %w", err)
-	}
-	return nil
+		if err != nil {
+			return fmt.Errorf("attach link error: %w", err)
+		}
+		return nil
+	*/
 }
 
 // DetachLinks func is used to detach links from kernel. and Unpin Maps. And After this call Close().
